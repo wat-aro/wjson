68f82a536c4655cdfe9ecc04759e455c04025567
wat-aro <kazutas1008@gmail.com>
2022-05-17 22:35:35 +0900

    Use number module

--- src/lib.rs
+++ src/lib.rs
@@ -1,106 +1,28 @@
-use nom::branch::alt;
-use nom::character::complete::char;
-use nom::combinator::map;
-use nom::multi::many1;
-use nom::IResult;
+pub mod number;
 
-fn zero(input: &str) -> IResult<&str, u64> {
-    map(char('0'), |c| c.to_string().parse::<u64>().unwrap())(input)
+use number::number;
+use number::Number;
+use std::error::Error;
+
+#[derive(Debug, PartialEq)]
+pub enum Value {
+    Number(Number),
 }
 
-fn onenine(input: &str) -> IResult<&str, u64> {
-    map(
-        alt((
-            char('1'),
-            char('2'),
-            char('3'),
-            char('4'),
-            char('5'),
-            char('6'),
-            char('7'),
-            char('8'),
-            char('9'),
-        )),
-        |c| c.to_string().parse::<u64>().unwrap(),
-    )(input)
-}
+pub fn parse<'a>(input: &'a str) -> Result<Value, Box<dyn Error + 'a>> {
+    let (_, result) = number(input)?;
 
-pub fn digit(input: &str) -> IResult<&str, u64> {
-    alt((zero, onenine))(input)
-}
-
-pub fn digits(input: &str) -> IResult<&str, u64> {
-    let (rest, v) = many1(digit)(input)?;
-    let str_vec: String = v.iter().map(|d| d.to_string()).collect::<String>();
-
-    Ok((rest, str_vec.parse().unwrap()))
+    Ok(Value::Number(result))
 }
 
 #[cfg(test)]
 mod tests {
-    use nom::error::{Error, ErrorKind};
-    use nom::Err;
-
     use super::*;
 
     #[test]
     fn parse_zero() {
-        assert_eq!(zero("0"), Ok(("", 0)));
-    }
-
-    #[test]
-    fn failed_parse_one() {
-        assert_eq!(zero("1"), Err(Err::Error(Error::new("1", ErrorKind::Char))))
-    }
-
-    #[test]
-    fn parse_one() {
-        assert_eq!(onenine("1"), Ok(("", 1)));
-    }
-
-    #[test]
-    fn parse_nine() {
-        assert_eq!(onenine("9"), Ok(("", 9)));
-    }
-
-    #[test]
-    fn failed_parse_a() {
-        assert_eq!(
-            digit("a"),
-            Err(Err::Error(Error::new("a", ErrorKind::Char)))
-        )
-    }
-
-    #[test]
-    fn digit_zero() {
-        assert_eq!(digit("0"), Ok(("", 0)));
-    }
-
-    #[test]
-    fn digit_one() {
-        assert_eq!(digit("1"), Ok(("", 1)));
-    }
-
-    #[test]
-    fn digit_one_nine() {
-        assert_eq!(digit("19"), Ok(("9", 1)));
-    }
-
-    #[test]
-    fn digit_one_alpha() {
-        assert_eq!(digit("1a"), Ok(("a", 1)));
-    }
-
-    #[test]
-    fn digit_alpha() {
-        assert_eq!(
-            digit("a"),
-            Err(Err::Error(Error::new("a", ErrorKind::Char)))
-        );
-    }
-
-    #[test]
-    fn digits1() {
-        assert_eq!(digits("123"), Ok(("", 123)))
+        if let Ok(value) = parse("0") {
+            assert_eq!(value, Value::Number(Number::PositiveInteger(0)))
+        }
     }
 }
---
+++ src/number.rs
@@ -0,0 +1,140 @@
+use nom::branch::alt;
+use nom::character::complete::char;
+use nom::combinator::map;
+use nom::multi::many1;
+use nom::IResult;
+
+#[derive(Debug, PartialEq)]
+pub enum Number {
+    PositiveInteger(u64),
+}
+
+/// Recognize "0"
+///
+/// ```rust
+/// use nom::error::{ErrorKind, Error};
+/// use nom::Err;
+/// use wjson::number::{number, Number};
+/// # fn main() {
+///
+///
+/// // the parser will parse "0"
+/// assert_eq!(number("0"), Ok(("", Number::PositiveInteger(0))));
+///
+/// // this will fail if number fails
+/// assert_eq!(number("1"), Err(Err::Error(Error::new("1", ErrorKind::Char))));
+/// # }
+/// ```
+pub fn number(input: &str) -> IResult<&str, Number> {
+    map(integer, |u: u64| Number::PositiveInteger(u))(input)
+}
+
+/// Recognize integer
+fn integer(input: &str) -> IResult<&str, u64> {
+    zero(input)
+}
+
+/// Recognize "0"
+fn zero(input: &str) -> IResult<&str, u64> {
+    map(char('0'), |c| c.to_string().parse::<u64>().unwrap())(input)
+}
+
+/// Recognize '1' ... '9'
+fn onenine(input: &str) -> IResult<&str, u64> {
+    map(
+        alt((
+            char('1'),
+            char('2'),
+            char('3'),
+            char('4'),
+            char('5'),
+            char('6'),
+            char('7'),
+            char('8'),
+            char('9'),
+        )),
+        |c| c.to_string().parse::<u64>().unwrap(),
+    )(input)
+}
+
+/// Recognize a digit
+fn digit(input: &str) -> IResult<&str, u64> {
+    alt((zero, onenine))(input)
+}
+
+/// Recognize digits
+fn digits(input: &str) -> IResult<&str, u64> {
+    let (rest, v) = many1(digit)(input)?;
+    let str_vec: String = v.iter().map(|d| d.to_string()).collect::<String>();
+
+    Ok((rest, str_vec.parse().unwrap()))
+}
+
+#[cfg(test)]
+mod tests {
+    use nom::error::{Error, ErrorKind};
+    use nom::Err;
+
+    use super::*;
+
+    #[test]
+    fn assert_zero() {
+        assert_eq!(zero("0"), Ok(("", 0)));
+    }
+
+    #[test]
+    fn failed_parse_one() {
+        assert_eq!(zero("1"), Err(Err::Error(Error::new("1", ErrorKind::Char))))
+    }
+
+    #[test]
+    fn parse_one() {
+        assert_eq!(onenine("1"), Ok(("", 1)));
+    }
+
+    #[test]
+    fn parse_nine() {
+        assert_eq!(onenine("9"), Ok(("", 9)));
+    }
+
+    #[test]
+    fn failed_parse_a() {
+        assert_eq!(
+            digit("a"),
+            Err(Err::Error(Error::new("a", ErrorKind::Char)))
+        )
+    }
+
+    #[test]
+    fn digit_zero() {
+        assert_eq!(digit("0"), Ok(("", 0)));
+    }
+
+    #[test]
+    fn digit_one() {
+        assert_eq!(digit("1"), Ok(("", 1)));
+    }
+
+    #[test]
+    fn digit_one_nine() {
+        assert_eq!(digit("19"), Ok(("9", 1)));
+    }
+
+    #[test]
+    fn digit_one_alpha() {
+        assert_eq!(digit("1a"), Ok(("a", 1)));
+    }
+
+    #[test]
+    fn digit_alpha() {
+        assert_eq!(
+            digit("a"),
+            Err(Err::Error(Error::new("a", ErrorKind::Char)))
+        );
+    }
+
+    #[test]
+    fn digits1() {
+        assert_eq!(Ok(("", 123)), digits("123"))
+    }
+}
